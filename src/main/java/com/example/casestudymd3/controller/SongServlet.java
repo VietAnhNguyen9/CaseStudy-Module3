7abcf7fd5aede504299a2918327adf63d02bacf8
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-08-31 10:09:32 +0700

    xoa template

--- src/main/java/com/example/casestudymd3/controller/SongServlet.java
+++ src/main/java/com/example/casestudymd3/controller/SongServlet.java
@@ -1,19 +1,88 @@
 package com.example.casestudymd3.controller;
 
+import com.example.casestudymd3.model.Songs;
+import com.example.casestudymd3.service.impl.SongService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "SongServlet", value = "/songServlet")
 public class SongServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private SongService songService;
 
+    @Override
+    public void init() throws ServletException {
+        songService = new SongService();
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "":
+                listSong(request, response);
+                break;
+            case "create":
+                createGetSong(request, response);
+                break;
+            case "update":
+                updateGetSong(request, response);
+                break;
+            case "sortByAlbum":
+                sortByAlbum(request, response);
+                break;
+            case "delete":
+                deleteSong(request, response);
+                break;
+        }
+    }
 
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                createGetSong(request, response);
+                break;
+            case "update":
+                updateGetSong(request, response);
+                break;
+            case "sortByAlbum":
+                sortByAlbum(request, response);
+                break;
+            case "delete":
+                deleteSong(request, response);
+                break;
+        }
+    }
+    private void deleteSong(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void sortByAlbum(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    private void updateGetSong(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void createGetSong(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void listSong(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Songs> songs = songService.findAll();
+        request.setAttribute("list", songs);
+        RequestDispatcher req = request.getRequestDispatcher("home.jsp");
+        req.forward(request, response);
     }
 }
